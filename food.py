fccf771e3fce5c8db95d3b74650ad507904a4ec7
AlinaDbeep <82227705+AlinaDbeep@users.noreply.github.com>
2021-12-13 03:23:37 +0200

    Update food.py

--- food.py
+++ food.py
@@ -8,7 +8,6 @@ class Food(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        #self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("purple")
         self.speed("fastest")
         self.refresh()
